c324904dc32823d5fd1b1b96e3c98587cbb977e3
Sudipto Sarker <sarker420@outlook.com>
2023-03-22 15:33:55 +0600

    Add project files.

---
+++ Engineer-Management/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+let csvArray = [];
+
+function getCSV() {
+    var csvFile = document.getElementById("csv").value;
+
+    let csv = new XMLHttpRequest();
+
+    // CSVファイルへのパス
+    csv.open("GET", csvFile, false);
+
+    // 改行ごとに配列化
+    let lines = csv.responseText.split(/\r\n|\n/);
+
+    alert(lines + " ");
+    // 1行ごとに処理
+    for (let i = 0; i < lines.length; ++i) {
+        let cells = lines[i].split(",");
+        if (cells.length != 1) {
+            csvArray.push(cells);
+        }
+    }
+    console.log(csvArray);
+}
+
+
